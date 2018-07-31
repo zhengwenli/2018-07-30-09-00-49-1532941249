dbcb689b3146fba3e9a89f4fb5ce33416b55c81c
zhengwenli <zhengwenli_95@163.com>
2018-07-31 20:49:23 +0800

    first try

--- main/main.js
+++ main/main.js
@@ -1,18 +1,50 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    o) 最小值 = ${sequence.maximum()}
+    o) 最小值 = ${sequence.serial_number()}
+    o) 最小值 = ${sequence.serial_mean()}`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    this.input = input
   }
 
   minimum() {
     // Write your code here
+    var arrmin = input[0];
+    for(var i = 1; i <input.length; i++){
+      if(input[i]<arrmin){
+        arrmin = input[i];
+      }
+    }
+    return arrmin;
   }
 
   // Write your code here
+  maximum(){
+    var arrmax = input[0];
+    for(var i = 1; i <input.length; i++){
+      if(input[i]>arrmax){
+        arrmax = input[i];
+      }
+    }
+    return arrmax; 
+  }
+
+  serial_number(){
+    return this.input.length;
+  }
+
+  serial_mean(){
+    var ave=0;
+    var sum=0;
+    for(var i = 1; i <input.length; i++){
+      sum +=input[i];
+    }
+    ave = (sum / input.length).toFixed(2);
+    return ave;
+  }
 }
